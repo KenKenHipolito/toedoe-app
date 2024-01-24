174f7cb7d36840bc9e76f54a5a0308bb56a8439b
Kennedy Hipolito <41745628+whoamikenken@users.noreply.github.com>
2024-01-25 02:36:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Taskspage from "./pages/TasksPage.vue";
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <Navbar/>
+  <Taskspage/>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
