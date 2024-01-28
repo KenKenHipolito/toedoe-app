0ba8d74a121307b226c3db1db6b400076f0d2907
Kennedy Hipolito <41745628+whoamikenken@users.noreply.github.com>
2024-01-28 17:47:22 +0800

    Update

---
+++ src/pages/errors/NotFoundErrorPage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container py-6">
+        <h1>404</h1>
+        <p>Sorry the page you're looking for doesn't exist</p>
+        <router-link to="/tasks" class="btn btn-primary">Back to Tasks</router-link>
+    </div>
+</template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,16 @@ import TaskPage from "../pages/TasksPage.vue"
 import LoginPage from "../pages/LoginPage.vue"
 import RegisterPage from "../pages/RegisterPage.vue"
 import SummaryPage from "../pages/SummaryPage.vue"
+import NotFoundErrorPage from "../pages/errors/NotFoundErrorPage.vue"
 
 const routes = [
     {
         path: "/tasks",
         component: TaskPage,
-        name: 'tasks'
+        name: 'tasks',
+        meta: {
+            auth:true
+        }
     },
     {
         path: "/login",
@@ -22,7 +26,15 @@ const routes = [
     {
         path: "/summary",
         component: SummaryPage,
-        name: 'summary'
+        name: 'summary',
+        meta: {
+            auth: true
+        }
+    },
+    {
+        path: "/:notFoung(.*)",
+        component: NotFoundErrorPage,
+        name: 'error.404'
     }
 ]
 
